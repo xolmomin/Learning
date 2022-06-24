333dde4b20b3ce5400858621e53be4d405504658
Ravshanjon <akhamdjonovr.98@gmail.com>
2022-06-24 21:19:32 +0500

    remove comments and add env folder to gitignore

--- config/settings.py
+++ config/settings.py
@@ -1,14 +1,8 @@
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure--_t5jhk+@39&4!h%_i94!vp6#&4m2#splr^i0vhwgt2x6@-7i1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
