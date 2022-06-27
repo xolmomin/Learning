60bd9eba4fa5ebf9853082ab40dbf52fcecb3174
shohruh <shohruh.abd2223@gmail.com>
2022-06-27 19:10:08 +0500

    template polni tayyor

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('edu.urls')),
 ]
-print('hellooo broo')
