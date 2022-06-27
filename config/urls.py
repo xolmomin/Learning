a19450847827aaada426e6fef08aaf21d8edb4f9
Ravshanjon <akhamdjonovr.98@gmail.com>
2022-06-27 19:12:37 +0500

    templateview and base html not full

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('', TemplateView.as_view(template_name='index.html'), name="home"),
+    path('html/', include('edu.urls')),
+
 ]
+
---
+++ edu/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('index/', TemplateView.as_view(template_name='home.html'), name="index"),
+    path('essential-buttons/', TemplateView.as_view(template_name="essential-buttons.html"), name="essential-buttons"),
+    path('essential-icons/', TemplateView.as_view(template_name="essential-icons.html"), name="essential-icons"),
+    path('essential-progress/', TemplateView.as_view(template_name="essential-progress.html"),
+         name="essential-progress"),
+    path('essential-grid/', TemplateView.as_view(template_name="essential-grid.html"), name="essential-grid"),
+    path('essential-forms/', TemplateView.as_view(template_name="essential-forms.html"), name="essential-forms"),
+    path('essential-tables/', TemplateView.as_view(template_name="essential-tables.html"), name="essential-tables"),
+    path('essential-tabs/', TemplateView.as_view(template_name="essential-tabs.html"), name="essential-tabs"),
+    path('essential-tabs/', TemplateView.as_view(template_name="essential-tabs.html"), name="essential-tabs"),
+    path('login/', TemplateView.as_view(template_name='login.html'), name="login"),
+    path('pricing/', TemplateView.as_view(template_name='pricing.html'), name="pricing"),
+    path('sign-up/', TemplateView.as_view(template_name='sign-up.html'), name="sign-up"),
+    path('survey/', TemplateView.as_view(template_name='survey.html'), name="survey"),
+    path('tutors/', TemplateView.as_view(template_name='tutors.html'), name="tutors"),
+]
