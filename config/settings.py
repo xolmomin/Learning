38ebe8470d516d5f050244f38ba4e056909b39e9
shohruh <shohruh.abd2223@gmail.com>
2022-07-01 20:40:32 +0500

    staticni qoshdim

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,124 @@ INSTALLED_APPS = [
     'apps.courses',
     'apps.billings',
 
-    # Third-party
+    # ... include the providers you want to enable:
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.agave',
+    'allauth.socialaccount.providers.amazon',
+    'allauth.socialaccount.providers.amazon_cognito',
+    'allauth.socialaccount.providers.angellist',
+    'allauth.socialaccount.providers.apple',
+    'allauth.socialaccount.providers.asana',
+    'allauth.socialaccount.providers.auth0',
+    'allauth.socialaccount.providers.authentiq',
+    'allauth.socialaccount.providers.azure',
+    'allauth.socialaccount.providers.baidu',
+    'allauth.socialaccount.providers.basecamp',
+    'allauth.socialaccount.providers.battlenet',
+    'allauth.socialaccount.providers.bitbucket',
+    'allauth.socialaccount.providers.bitbucket_oauth2',
+    'allauth.socialaccount.providers.bitly',
+    'allauth.socialaccount.providers.box',
+    'allauth.socialaccount.providers.cern',
+    'allauth.socialaccount.providers.cilogon',
+    'allauth.socialaccount.providers.coinbase',
+    'allauth.socialaccount.providers.dataporten',
+    'allauth.socialaccount.providers.daum',
+    'allauth.socialaccount.providers.digitalocean',
+    'allauth.socialaccount.providers.discord',
+    'allauth.socialaccount.providers.disqus',
+    'allauth.socialaccount.providers.douban',
+    'allauth.socialaccount.providers.doximity',
+    'allauth.socialaccount.providers.draugiem',
+    'allauth.socialaccount.providers.drip',
+    'allauth.socialaccount.providers.dropbox',
+    'allauth.socialaccount.providers.dwolla',
+    'allauth.socialaccount.providers.edmodo',
+    'allauth.socialaccount.providers.edx',
+    'allauth.socialaccount.providers.eventbrite',
+    'allauth.socialaccount.providers.eveonline',
+    'allauth.socialaccount.providers.evernote',
+    'allauth.socialaccount.providers.exist',
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.feedly',
+    'allauth.socialaccount.providers.figma',
+    'allauth.socialaccount.providers.fivehundredpx',
+    'allauth.socialaccount.providers.flickr',
+    'allauth.socialaccount.providers.foursquare',
+    'allauth.socialaccount.providers.frontier',
+    'allauth.socialaccount.providers.fxa',
+    'allauth.socialaccount.providers.gitea',
+    'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.gitlab',
+    'allauth.socialaccount.providers.globus',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.gumroad',
+    'allauth.socialaccount.providers.hubic',
+    'allauth.socialaccount.providers.instagram',
+    'allauth.socialaccount.providers.jupyterhub',
+    'allauth.socialaccount.providers.kakao',
+    'allauth.socialaccount.providers.keycloak',
+    'allauth.socialaccount.providers.lemonldap',
+    'allauth.socialaccount.providers.line',
+    'allauth.socialaccount.providers.linkedin',
+    'allauth.socialaccount.providers.linkedin_oauth2',
+    'allauth.socialaccount.providers.mailchimp',
+    'allauth.socialaccount.providers.mailru',
+    'allauth.socialaccount.providers.mediawiki',
+    'allauth.socialaccount.providers.meetup',
+    'allauth.socialaccount.providers.microsoft',
+    'allauth.socialaccount.providers.naver',
+    'allauth.socialaccount.providers.nextcloud',
+    'allauth.socialaccount.providers.odnoklassniki',
+    'allauth.socialaccount.providers.openid',
+    'allauth.socialaccount.providers.openstreetmap',
+    'allauth.socialaccount.providers.orcid',
+    'allauth.socialaccount.providers.patreon',
+    'allauth.socialaccount.providers.paypal',
+    'allauth.socialaccount.providers.persona',
+    'allauth.socialaccount.providers.pinterest',
+    'allauth.socialaccount.providers.pocket',
+    'allauth.socialaccount.providers.quickbooks',
+    'allauth.socialaccount.providers.reddit',
+    'allauth.socialaccount.providers.robinhood',
+    'allauth.socialaccount.providers.salesforce',
+    'allauth.socialaccount.providers.sharefile',
+    'allauth.socialaccount.providers.shopify',
+    'allauth.socialaccount.providers.slack',
+    'allauth.socialaccount.providers.snapchat',
+    'allauth.socialaccount.providers.soundcloud',
+    'allauth.socialaccount.providers.spotify',
+    'allauth.socialaccount.providers.stackexchange',
+    'allauth.socialaccount.providers.steam',
+    'allauth.socialaccount.providers.stocktwits',
+    'allauth.socialaccount.providers.strava',
+    'allauth.socialaccount.providers.stripe',
+    'allauth.socialaccount.providers.telegram',
+    'allauth.socialaccount.providers.trainingpeaks',
+    'allauth.socialaccount.providers.trello',
+    'allauth.socialaccount.providers.tumblr',
+    'allauth.socialaccount.providers.twentythreeandme',
+    'allauth.socialaccount.providers.twitch',
+    'allauth.socialaccount.providers.twitter',
+    'allauth.socialaccount.providers.untappd',
+    'allauth.socialaccount.providers.vimeo',
+    'allauth.socialaccount.providers.vimeo_oauth2',
+    'allauth.socialaccount.providers.vk',
+    'allauth.socialaccount.providers.weibo',
+    'allauth.socialaccount.providers.weixin',
+    'allauth.socialaccount.providers.windowslive',
+    'allauth.socialaccount.providers.xing',
+    'allauth.socialaccount.providers.yahoo',
+    'allauth.socialaccount.providers.yandex',
+    'allauth.socialaccount.providers.ynab',
+    'allauth.socialaccount.providers.zoho',
+    'allauth.socialaccount.providers.zoom',
+    'allauth.socialaccount.providers.okta',
+    'allauth.socialaccount.providers.feishu',
 
 
 ]
@@ -67,16 +184,6 @@ AUTH_USER_MODEL = 'users.User'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#         # 'NAME': 'learning',
-#         # 'USER': os.environ.get("POSTGRES_USER"),
-#         # 'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
-#         # 'HOST': 'localhost',
-#         # 'PORT': '5432',
-#     }
 DATABASES = {
     'default':{
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -89,8 +196,6 @@ DATABASES = {
 }
 
 
-
-# Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -137,6 +242,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+#smtp
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -148,3 +255,58 @@ LOGIN_URL = '/'
 LOGIN_REDIRECT_URL = '/'
 
 PASSWORD_RESET_TIMEOUT = 3600
+
+
+#django allauth
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+# Provider specific settings
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        # For each OAuth based provider, either add a ``SocialApp``
+        # (``socialaccount`` app) containing the required client
+        # credentials, or list them here:
+        'APP': {
+            'client_id': '856489160437-3qe1ftmu6teq8tchelvs201sdpsdpcri.apps.googleusercontent.com',
+            'secret': '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',
+            'key': ''
+        },
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    },
+    'facebook': {
+        'METHOD': 'oauth2',
+        'SDK_URL': '//connect.facebook.net/{locale}/sdk.js',
+        'SCOPE': ['email', 'public_profile'],
+        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+        'INIT_PARAMS': {'cookie': True},
+        'FIELDS': [
+            'id',
+            'first_name',
+            'last_name',
+            'middle_name',
+            'name',
+            'name_format',
+            'picture',
+            'short_name'
+        ],
+        'EXCHANGE_TOKEN': True,
+        'LOCALE_FUNC': lambda request: 'en_US',
+        'VERIFIED_EMAIL': False,
+        'VERSION': 'v13.0',
+    }
+}
--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.urls'))
+    path('', include('apps.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
