45e902535d71fe29308b941cfe3e65203e6fe05d
Ravshanjon <akhamdjonovr.98@gmail.com>
2022-07-04 20:30:02 +0500

    create usermodel migration from zero

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,19 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-# from mptt.admin import MPTTModelAdmin, DraggableMPTTAdmin
-#
-# from apps.courses.models import Course
-#
-#
-# @admin.register(Course)
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'title', 'image', 'author', 'duration', 'is_active']
-#     fields = ['title', 'price', 'description', 'image', 'author', 'duration']
-#
-#
-# class CustomMPTTModelAdmin(DraggableMPTTAdmin):
-#     # specify pixel amount for this ModelAdmin only:
-#     mptt_level_indent = 20
-#
-#
-# admin.site.register(Course, CustomMPTTModelAdmin)
+
+from apps.users.models import User
+
+admin.site.register(User)
