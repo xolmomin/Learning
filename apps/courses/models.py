33db82e135d27db35c3b20f2565fce3f8972f834
Ravshanjon <akhamdjonovr.98@gmail.com>
2022-07-04 15:41:43 +0500

    created base models for course app

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.db.models import Model, DateTimeField, TextField
 
-# Create your models here.
+
+class BaseModel(Model):
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+    description = TextField(null=True, blank=True)
+
+    class Meta:
+        abstract=True
+
+
+class Course:
+    pass
+
+
