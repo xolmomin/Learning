86c62d0eec4a192c7164d0bdd3de6caf4e07c5fb
Ravshanjon <akhamdjonovr.98@gmail.com>
2022-07-06 19:28:02 +0500

    register models from course app admin page

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from courses.models import CourseCategory, Course, FeedBack, Chapter, Lesson, Tag, Comment, Question, Answer, \
+    ForumCategory, Forum
+
+admin.site.register([FeedBack, Chapter,Comment,  ForumCategory, Forum])
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 20
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 4
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline]
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    pass
+
+class QuizAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(CourseCategory)
+class CourseCategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
